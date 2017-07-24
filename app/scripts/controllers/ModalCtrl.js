2bde59137937e376265305aa51eccc741700842c
rtrunkhill <rtrunkhill@gmail.com>
2017-07-24 15:44:44 -0700

    4pm stop issue w/

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,9 +1,18 @@
 (function() {
-  function ModalCtrl() {
+  function ModalCtrl(Room, $uibModalInstance) {
+    this.cancel = function() {
+      $uibModalInstance.dismiss();
+    };
+    this.submit = function() {
+      Room.add();
+      $uibModalInstance.close()
+    };
 
   }
 
   angular
-  .module('blocChat')
-  .controller('ModalCtrl', [ModalCtrl]);
-})()
+  .module('blocChat', ['ui.bootstrap'])
+  .controller('ModalCtrl', ['Room', $uibModalInstance, ModalCtrl]);
+})();
+
+// will need a submit and cancel button
